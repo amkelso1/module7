b3d608db57947b2234d8087e0f60bba62ffcbf5e
amkelso1 <amkelso1@dmacc.edu>
2020-10-10 14:39:12 -0500

    made basic input return function and a test

---
+++ fun_with_collections/basic_list_exception.py
@@ -0,0 +1,23 @@
+
+def make_list():
+    u_list = []
+    for i in range(0, 3):
+        try:
+            u_input = int(input())
+            if u_input < 1 or u_input > 50:
+                raise ValueError
+        except ValueError:
+            raise ValueError
+        else:
+            u_list.insert(i, u_input)
+    return u_list
+
+
+def get_input():
+    u_input = input("give me a number: ")
+    return u_input
+
+
+if __name__ == '__main__':
+    test_list = make_list()
+    print(test_list)
---
+++ fun_with_collections/basic_lists.py
@@ -0,0 +1,24 @@
+"""
+Author: Alex Kelso
+Program: basic_lists.py
+
+purpose: demonstrate input and output through functions
+"""
+
+
+def make_list():
+    u_list = []
+    for i in range(0, 3):
+        u_input = int(input())
+        u_list.insert(i, u_input)
+    return u_list
+
+
+def get_input():
+    u_input = input("give me a number: ")
+    return u_input
+
+
+if __name__ == '__main__':
+    test_list = make_list()
+    print(test_list)
