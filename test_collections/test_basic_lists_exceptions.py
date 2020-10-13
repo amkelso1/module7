63ffbd11d07398bee89348c1bb06a0d694b574d4
amkelso1 <amkelso1@dmacc.edu>
2020-10-13 14:22:55 -0500

    made basic input return function and a test(failed)

--- test_collections/test_basic_lists_exceptions.py
+++ test_collections/test_basic_lists_exceptions.py
@@ -3,31 +3,25 @@ from unittest.mock import patch
 from fun_with_collections import basic_list_exception
 
 
-class MyTestCase(unittest.TestCase):
-    @patch('fun_with_collections.basic_lists_exception.get_input', return_value='5')
+class TestList(unittest.TestCase):
+    @patch('fun_with_collections.basic_list_exception.get_input', return_value='5')
     def test_make_list(self, input):
         self.assertEqual(basic_list_exception.make_list(), [5, 5, 5])
 
-
-class TestList_nonNumeric(unittest.TestCase):
-    @patch('fun_with_collections.basic_lists_exception.get_input', return_value='ab')
+    @patch('fun_with_collections.basic_list_exceptions.get_input', return_value='ab')  # patch function for input
     def test_make_list_non_numeric(self, input):  # pass input
         with self.assertRaises(ValueError):  # this is familiar
-            basic_list_exception.make_list()  # call the function
+            basic_list_exception.make_list()  # call the function!
 
-
-class TestList_below(unittest.TestCase):
-    @patch('fun_with_collections.basic_lists_exception.get_input', return_value='-4')
+    @patch('fun_with_collections.basic_list_exceptions.get_input', return_value='-4')  # patch function for input
     def test_make_list_below_range(self, input):  # pass input
         with self.assertRaises(ValueError):  # this is familiar
-            basic_list_exception.make_list()  # call the function
+            basic_list_exception.make_list()  # call the function!
 
-
-class TestList_above(unittest.TestCase):
-    @patch('fun_with_collections.basic_lists_exception.get_input', return_value='52')
+    @patch('fun_with_collections.basic_list_exceptions.get_input', return_value='51')  # patch function for input
     def test_make_list_above_range(self, input):  # pass input
         with self.assertRaises(ValueError):  # this is familiar
-            basic_list_exception.make_list()  # call the function
+            basic_list_exception.make_list()  # call the function!
 
 
 if __name__ == '__main__':
